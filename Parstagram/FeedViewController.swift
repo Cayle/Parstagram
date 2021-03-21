955a7ac81486561ec48c1283ee77073e85acef9f
Cayle <calebabhulimhen@gmail.com>
2021-03-21 12:55:09 -0500

    implemented pull to refresh and also made the most recent post show up first.

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -16,31 +16,51 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     @IBOutlet weak var tableView: UITableView!
     
+    let myRefreshControl = UIRefreshControl()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadPictures()
         tableView.delegate = self
         tableView.dataSource = self
 
         // Do any additional setup after loading the view.
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
         
+        myRefreshControl.addTarget(self, action: #selector(loadPictures), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
+    }
+      
+    @objc func loadPictures() {
         let query = PFQuery(className: "Posts")
         query.includeKey("author")
         query.limit = 20
         
         query.findObjectsInBackground { (posts, error) in
-            if posts != nil {
-                self.posts = posts!
-                self.tableView.reloadData()
-            } else {
-                print("Error occured \(String(describing: error))")
+        if posts != nil {
+            self.posts = posts!
+            self.tableView.reloadData()
+            self.myRefreshControl.endRefreshing()
+        } else {
+            print("Error occured \(String(describing: error))")
             }
         }
     }
+//    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//
+//        let query = PFQuery(className: "Posts")
+//        query.includeKey("author")
+//        query.limit = 20
+//
+//        query.findObjectsInBackground { (posts, error) in
+//            if posts != nil {
+//                self.posts = posts!
+//                self.tableView.reloadData()
+//            } else {
+//                print("Error occured \(String(describing: error))")
+//            }
+//        }
+//    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return posts.count
@@ -48,7 +68,8 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
-        let post = posts[indexPath.row]
+        let length = posts.count
+        let post = posts[length - 1 - indexPath.row]
         let user = post["author"] as! PFUser
         cell.usernameLabel.text = user.username
         cell.captionLabel.text = (post["caption"] as! String)
